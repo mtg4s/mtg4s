2be44dfde7c61e0124e790ad374e6a9f4cd626a7
Gabor Pihaj <gabor.pihaj@gmail.com>
2020-04-06 20:11:27 +0100

    Refactor DeckboxCsvParser

--- inventory/src/main/scala/vdx/mtg4s/inventory/parser/Parser.scala
+++ inventory/src/main/scala/vdx/mtg4s/inventory/parser/Parser.scala
@@ -1,14 +1,27 @@
 package vdx.mtg4s.inventory.parser
 
+import cats.Applicative
 import cats.data.NonEmptyList
 import vdx.mtg4s.inventory.Inventory
-import vdx.mtg4s.inventory.parser.Parser.ParserError
+import vdx.mtg4s.inventory.parser.Parser.ParserResult
 
 trait Parser[F[_]] {
-  def parse(raw: String): F[Either[NonEmptyList[ParserError], Inventory]]
+  def parse(raw: String): F[ParserResult[Inventory]]
 }
 
 object Parser {
+  type ParserResult[A] = Either[NonEmptyList[ParserError], A]
+
   sealed trait ParserError
   final case class ParsingError(message: String) extends ParserError
+
+  private[parser] def oneError[A](error: ParserError): NonEmptyList[ParserError] =
+    NonEmptyList.one(error)
+
+  private[parser] def errorResult[A](error: ParserError): ParserResult[A] =
+    Left(oneError(error))
+
+  private[parser] def errorResultF[F[_]: Applicative, A](error: ParserError): F[ParserResult[A]] =
+    Applicative[F].pure(errorResult(error))
+
 }
--- inventory/src/main/scala/vdx/mtg4s/inventory/parser/deckbox/DeckboxCsvParser.scala
+++ inventory/src/main/scala/vdx/mtg4s/inventory/parser/deckbox/DeckboxCsvParser.scala
@@ -1,12 +1,10 @@
 package vdx.mtg4s.inventory.parser.deckbox
 
 import cats.data.Chain
-import cats.data.NonEmptyList
 import cats.effect.Sync
 import cats.instances.either._
 import cats.syntax.flatMap._
 import cats.syntax.functor._
-import cats.syntax.semigroup._
 import cats.syntax.traverse._
 import kantan.csv._
 import kantan.csv.ops._
@@ -17,31 +15,30 @@ import vdx.mtg4s.inventory.parser.Parser
 import vdx.mtg4s.inventory.parser.Parser._
 
 object DeckboxCsvParser {
-  def apply[F[_]: Sync, Repr](db: CardDB[F, Repr])(implicit G: Getter[Repr, MtgJsonId]): Parser[F] =
-    raw =>
+
+  def apply[F[_]: Sync, Repr](db: CardDB[F, Repr])(implicit G: Getter[Repr, MtgJsonId]): Parser[F] = new Parser[F] {
+    override def parse(raw: String): F[ParserResult[Inventory]] =
       Sync[F]
         .delay(raw.trim.asCsvReader[RawDeckboxCard](rfc.withHeader(true)))
-        .map(s => s.foldLeft[Chain[ReadResult[RawDeckboxCard]]](Chain.empty)(_ |+| Chain.one(_)))
         .flatMap(
-          _.map(
-            _.fold[F[Either[NonEmptyList[ParserError], InventoryItem]]](
-              _ => Sync[F].pure(Left(NonEmptyList.one(ParsingError("Error parsing line")))),
-              item =>
-                db.find(CardName(item.name), Set("foobar"))
-                  .map(
-                    _.fold[Either[NonEmptyList[ParserError], InventoryItem]](
-                      Left(NonEmptyList.one(ParsingError("Cannot find card in db")))
-                    )(r => Right(reprToInventoryItem(item.count)(r)))
-                  )
+          _.foldLeft[Chain[F[ParserResult[InventoryItem]]]](Chain.empty) { (chain, result) =>
+            chain ++ Chain.one(
+              result.fold(
+                _ => errorResultF[F, InventoryItem](ParsingError("Error parsing line")),
+                item => findOrParsingError(CardName(item.name), Set("foobar"), item.count)
+              )
             )
-          ).traverse(identity)
-            .map(
-              _.traverse(identity)
-            )
+          }.traverse(identity)
+            .map(_.traverse(identity))
         )
 
-  private[this] def reprToInventoryItem[Repr](count: Int)(r: Repr)(implicit G: Getter[Repr, MtgJsonId]): InventoryItem =
-    InventoryItem(G.get(r), count)
+    private[this] def findOrParsingError(name: CardName, set: Set, count: Int): F[ParserResult[InventoryItem]] =
+      db.find(name, set)
+        .map(
+          _.toRight(oneError(ParsingError("Cannot find card in db")))
+            .map(r => InventoryItem(G.get(r), count))
+        )
+  }
 
   private[deckbox] final case class RawDeckboxCard(
     count: Int,
